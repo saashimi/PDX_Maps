640349eac1074213f801aff5e1c7b43176546dac
saashimi <saavedra.kevin@gmail.com>
2015-11-13 13:38:22 -0800

    last known working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-    
+var map = null;    
+var trafficLayer=new google.maps.TrafficLayer();
+
+
+
 function trimet() {
   var url = "https://developer.trimet.org/ws/v2/vehicles/appID=" 
   var dataOut = [];
@@ -8,7 +12,7 @@ function trimet() {
     $.each(data, function(index, value) {
       innerData = data[index];
       $.each(innerData, function(index1, value1) {
-        if (index1 === "routeNumber" && value1 === 35) {
+        if (index1 === "routeNumber" && value1 === 8) {
           var coord = [innerData.latitude, innerData.longitude];
           dataOut.push(coord);
         }
@@ -20,25 +24,43 @@ function trimet() {
 };
 
 function initialize(dataIn) {
+  
+  var styles = [
+    {
+       stylers: [
+        //{hue : "#084C8D" },
+        {saturation : -75}
+      ]
+    },{
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {lightness : 100}
+          //{visibility : "on"}
+        ]
+    }/*,{
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          {visibility : "off"}
+        ]
+    }*/
+  ];
+
   var mapProp = {
     center:new google.maps.LatLng(45.5200,-122.6819),
     zoom:11,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  
-  var markers = dataIn;
-
-  var styles = [
-    {
-      featureType: "all",
-      elementType: "all",
-      stylers: [
-        {saturation : -50}
-      ]
+    mapTypeControlOptions: {  
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'desaturated']
     }
-  ];
+  };
+  map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var mapType=new google.maps.StyledMapType(styles, {name : 'Desaturated'});
+  map.mapTypes.set('desaturated', mapType);
+  map.setMapTypeId('desaturated');
+
+  var markers = dataIn;
+ // trafficLayer.setMap(null);
 
   // refactor for JQuery later
   for( i = 0; i < markers.length; i++ ) {
@@ -49,9 +71,18 @@ function initialize(dataIn) {
         map: map
     });
   }
+  check();
 };
+
+function check() {
+  if (document.getElementById('traffic').checked) {
+    trafficLayer.setMap(map);
+  } else {
+    trafficLayer.setMap(null);
+  }
+}
 
 trimet();
 
-//google.maps.event.addDomListener(window, 'load', initialize);     
+
 
